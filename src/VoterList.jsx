d43816ff627d6ba4057f6df37039ffbcdcb82025
HassamChundrigar <hassam.chundrigar520@gmail.com>
2022-08-17 22:06:00 +0500

    removed data file

--- src/VoterList.jsx
+++ src/VoterList.jsx
@@ -13,10 +13,12 @@ const VoterList = () => {
   const [scheme, setScheme] = useState([]);
   useEffect(() => {
     console.log("Use Effect");
-    fetch("Data.json").then((response) => {
-      // setVoterList(response);
-      response.json().then((data) => setVoterList(data));
-    });
+    fetch("https://storage.cloud.google.com/voterlists/Data.json").then(
+      (response) => {
+        // setVoterList(response);
+        response.json().then((data) => setVoterList(data));
+      }
+    );
     fetch("Scheme.json").then((response) => {
       // setVoterList(response);
       response.json().then((data) => setSchemeList(data));
